092e73483d9dbd279d25d9720b5e9173f710b2e4
Przemysław Rakowski <przemek.rakowski@gmail.com>
2020-12-21 00:53:44 +0100

    cleanup

--- src/scene/mesh/index.js
+++ src/scene/mesh/index.js
@@ -4,41 +4,22 @@ import vertexShader from './first.vert'
 import fragmentShader from './first.frag'
 
 const createGrid = async (uniforms) => {
-    const geometry = new THREE.PlaneGeometry(30, 5, 550, 550);
+    const geometry = new THREE.PlaneGeometry(30, 30, 30, 30);
 
-    const material = new THREE.ShaderMaterial({
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader,
-        uniforms
+    const material = new THREE.MeshPhongMaterial({
+        color: 0x111111
     });
 
     const grid = new THREE.Mesh(geometry, material);
-    // grid.rotation.x = -20
+    grid.rotation.x = Math.PI / 2;
     grid.material.side = THREE.DoubleSide
     return grid
 }
 
-const loadDonut = () => {
-    const loader = new GLTFLoader();
-    return new Promise((resolve, reject) => {
-    
-        loader.load('assets/donut.glb', (gltf) => {
-            // console.log(gltf.scene.children[2])
-            gltf.scene.children[2].material = new THREE.MeshPhongMaterial({
-                color: 0xffffff,
-            })
-            resolve(gltf.scene.children[2])
-        }, undefined, (err) => {
-            // console.log(err)
-            reject(err)
-        })
-    })
-}
 
 // register mesh
 export const getMesh = async (uniforms) => {
     return {
         grid: await createGrid(uniforms),
-        donut: await loadDonut()
     }
 }
