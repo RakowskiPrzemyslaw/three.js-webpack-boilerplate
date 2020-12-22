a94fda5e31e687c6b633721f7e24879bbc340d53
Przemysław Rakowski <przemek.rakowski@gmail.com>
2020-12-22 14:09:17 +0100

    remove physics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { loadScene } from 'src/scene'
 import { draw } from 'src/sketch'
 import { handleResize } from 'src/lib'
-import * as CANNON from 'cannon'
 
 const getRenderer = () => {
     const renderer = new THREE.WebGLRenderer({ 
@@ -18,7 +17,7 @@ const getRenderer = () => {
 
 const getCamera = () => {
     const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.0001, 10000);
-    camera.position.set(0, 40, 40);
+    camera.position.set(0, 0, 20);
     return camera
 }
 
@@ -26,41 +25,14 @@ const getControls = (camera, renderer) => {
     return new OrbitControls(camera, renderer.domElement);
 }
 
-
-const initCannon = () => {
- 
-    const world = new CANNON.World();
-    world.gravity.set(0, 0, 0);
-    world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 10;
-
-    const shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
-    const mass = 1;
-    const body = new CANNON.Body({
-        mass: 1
-    });
-    body.addShape(shape);
-    body.angularVelocity.set(0, 100, 100);
-    body.angularDamping = 0.5;
-    world.addBody(body);
-
-    return {
-        world, 
-        body
-    }
-}
-
-
-function loop({ scene, camera, renderer, controls, uniforms, updateUniforms, updatePhysics }) {
+function loop({ scene, camera, renderer, controls, uniforms, updateUniforms }) {
     updateUniforms()
-    updatePhysics(scene)
     draw({ scene, camera, renderer, controls, uniforms })
     renderer.render(scene, camera);
-    requestAnimationFrame(() => loop({ scene, camera, renderer, controls, uniforms, updateUniforms, updatePhysics }));
+    requestAnimationFrame(() => loop({ scene, camera, renderer, controls, uniforms, updateUniforms }));
 } 
 
 async function main() {
-    const timeStep = 1 / 60;
     const renderer = getRenderer()
     const camera = getCamera()
     const controls = getControls(camera, renderer)
@@ -75,31 +47,9 @@ async function main() {
         uniforms.u_time.value = clock.getElapsedTime()
     }
 
-    
     const scene = await loadScene(uniforms);
-    const { world, body } = initCannon()
 
-
-
-
-    const updatePhysics = (scene) => {
-        const mesh = scene.children[1]
-
-        // Step the physics world
-        world.step(timeStep);
-
-        // Copy coordinates from Cannon.js to Three.js
-        mesh.position.copy(body.position);
-        mesh.quaternion.copy(body.quaternion);
-
-    }
-
-
-    
-    const axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper)
-
-    loop({scene, camera, renderer, controls, uniforms, clock, updateUniforms, updatePhysics})
+    loop({scene, camera, renderer, controls, uniforms, clock, updateUniforms})
     document.body.appendChild(renderer.domElement);
     window.addEventListener('resize', () => handleResize(camera, renderer));
 }
--- src/scene/mesh/index.js
+++ src/scene/mesh/index.js
@@ -12,23 +12,14 @@ const createGrid = async (uniforms) => {
 
     const grid = new THREE.Mesh(geometry, material);
     grid.rotation.x = Math.PI / 2;
-    grid.position.y = -10;
     grid.material.side = THREE.DoubleSide
     return grid
 }
 
-const createBox = () => {
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-
-    const mesh = new THREE.Mesh(geometry, material);
-    return mesh
-}
 
 // register mesh
 export const getMesh = async (uniforms) => {
     return {
         grid: await createGrid(uniforms),
-        box: await createBox()
     }
 }
